3758c772b65eef6a8b6124fe0414c5bfa63b6cbf
KurakinPavel <pu.kurakin@yandex.ru>
2026-09-13 13:54:52 +0300

    first

--- parser.js
+++ parser.js
@@ -1,8 +1,29 @@
 // @todo: напишите здесь код парсера
 
 function parsePage() {
+
+    function getOpenGraph() {
+        return [...document.querySelectorAll('meta[property^="og:"]')]
+            .reduce((acc, tag) => {
+            const prop = tag.getAttribute('property');
+            const content = tag.getAttribute('content');
+            if (prop && content) {
+                acc[prop.replace(/^og:/, '')] = content;
+            }
+            return acc;
+        }, {});
+    }
+
     return {
-        meta: {},
+        meta: {
+            "language": document.querySelector("html").lang,
+            "title": document.querySelector('head title').textContent.split('—')[0].trim(),
+            "keywords": document.querySelector('meta[name="keywords"]').getAttribute('content')
+                .split(','),
+            "description": document.querySelector('meta[name="description"]').getAttribute('content'),
+            "opengraph": getOpenGraph()
+
+        },
         product: {},
         suggested: [],
         reviews: []
